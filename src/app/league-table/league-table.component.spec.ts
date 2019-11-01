b6863b5c141a2e11920629db0bc252ff8387d060
bugrain <neil@bugrain.plus.com>
2019-11-01 16:32:56 +0000

    feat(tables): Add league table component

---
+++ src/app/league-table/league-table.component.spec.ts
@@ -0,0 +1,31 @@
+//import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+//
+//import { LeagueTableComponent } from './league-table.component';
+//
+//import { LeagueServiceMock } from '../mocks/league.service.mock';
+//
+//describe('LeagueTableComponent', () => {
+//  let component: LeagueTableComponent;
+//  let fixture: ComponentFixture<LeagueTableComponent>;
+//
+//  beforeEach(async(() => {
+//    TestBed.configureTestingModule({
+//      declarations: [ LeagueTableComponent ],
+//  		imports: [LeagueServiceMock],
+//      providers: [LeagueServiceMock]
+//    })
+//    .compileComponents();
+//  }));
+//
+//  beforeEach(() => {
+//    fixture = TestBed.createComponent(LeagueTableComponent);
+//    component = fixture.componentInstance;
+//    fixture.detectChanges();
+//  });
+//
+//  it('should create', () => {
+//    expect(component).toBeTruthy();
+//  });
+//
+//});
+//
---
+++ src/app/league-table/league-table.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, FormControl, ValidatorFn } from '@angular/forms';
+
+import { LeagueService } from '../league.service';
+
+@Component({
+  selector: 'league-table',
+  templateUrl: './league-table.component.html',
+  styleUrls: ['./league-table.component.css']
+})
+export class LeagueTableComponent implements OnInit {
+  selectForm: FormGroup;
+  leaguesList;
+  gotData: boolean = false;
+  league;
+  divisions;
+  team;
+
+  constructor(private leagueService: LeagueService, private formBuilder: FormBuilder) {
+    this.selectForm = this.formBuilder.group({
+      leaguesList: ['']
+    });
+    
+    this.getLeaguesList();
+  }
+
+  ngOnInit() {
+  	this.onChanges();
+  }
+
+  getLeaguesList() {
+    this.leagueService.getLeaguesList()
+      .subscribe((data) => {
+        console.log(data);
+        this.leaguesList = data;
+        }, err => {
+          console.log(err);
+        }
+      );
+  }
+
+  getLeaguesData(id) {
+    this.leagueService.getTables(id)
+      .subscribe((data) => {
+        console.log('', data);
+        this.league = data['league'];
+        this.gotData = true;
+        console.log('league', this.league);
+        console.log('division', data['league']['divisions']);
+        console.log('team', data['league']['divisions'][0]['teams'][0]);
+        }, err => {
+          console.log(err);
+        }
+      );
+  }
+
+	onChanges(): void {
+	  this.selectForm.valueChanges.subscribe(values => {
+	    console.log(values);
+    this.getLeaguesData(values.leaguesList);
+	  });
+	}
+
+  submit() {
+    console.log(this.selectForm.value);
+    this.getLeaguesData(this.selectForm.value.leaguesList);
+  }
+
+}
