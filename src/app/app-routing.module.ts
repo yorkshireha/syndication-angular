bbab83c34547e91991441625d2f3c0b1ed41e19f
bugrain <neil@bugrain.plus.com>
2019-11-08 00:23:57 +0000

    feature(clubs&venues): Add club and venu views/components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LeagueTableComponent } from './league-table/league-table.component';
 import { FixturesComponent } from './fixtures/fixtures.component';
+import { ClubsComponent } from './clubs/clubs.component';
+import { VenuesComponent } from './venues/venues.component';
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'tables', component: LeagueTableComponent },
+  { path: 'dashboard',  component: DashboardComponent },
+  { path: 'tables',     component: LeagueTableComponent },
   { path: 'tables/:id', component: LeagueTableComponent },
-  { path: 'fixtures', component: FixturesComponent },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+  { path: 'fixtures',   component: FixturesComponent },
+  { path: 'clubs',      component: ClubsComponent },
+  { path: 'clubs/:id',  component: ClubsComponent },
+  { path: 'venues',     component: VenuesComponent },
+  { path: 'venues/:id', component: VenuesComponent },
+  { path: '',           redirectTo: '/dashboard', pathMatch: 'full' }
 ];
 
 
